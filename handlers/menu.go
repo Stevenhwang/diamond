030549bba0526707907572eee4a626dac6c87572
zed <90huanw@gmail.com>
2021-07-16 15:51:37 +0800

    menu handler

---
+++ handlers/menu.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+// 获取前端菜单列表
+func MenuList(c *fiber.Ctx) error { return nil }
+
+// 更新前端菜单信息
+func UpdateMenu(c *fiber.Ctx) error { return nil }
+
+// 新建前端菜单
+func CreateMenu(c *fiber.Ctx) error { return nil }
+
+// 删除前端菜单
+func DeleteMenu(c *fiber.Ctx) error { return nil }
