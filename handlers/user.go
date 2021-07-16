338f6e4ec4b45badd6915d7c35e1d3aaeee4d6e1
zed <90huanw@gmail.com>
2021-07-16 15:49:43 +0800

    user handler

---
+++ handlers/user.go
@@ -0,0 +1,24 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+// 用户登录
+func Login(c *fiber.Ctx) error { return nil }
+
+// 用户登出
+func Logout(c *fiber.Ctx) error { return nil }
+
+// 用户重置密码
+func ResetPasswd(c *fiber.Ctx) error { return nil }
+
+// 获取用户列表
+func UserList(c *fiber.Ctx) error { return nil }
+
+// 更新用户信息
+func UpdateUser(c *fiber.Ctx) error { return nil }
+
+// 新建用户
+func CreateUser(c *fiber.Ctx) error { return nil }
+
+// 删除用户
+func DeleteUser(c *fiber.Ctx) error { return nil }
