155a89a2a066ec82bec495d4ec962f0af903466a
zed <90huanw@gmail.com>
2021-07-16 17:32:44 +0800

    add handler

---
+++ handlers/group.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+// 获取服务器分组列表
+func GroupList(c *fiber.Ctx) error { return nil }
+
+// 更新服务器分组信息
+func UpdateGroup(c *fiber.Ctx) error { return nil }
+
+// 新建服务器分组
+func CreateGroup(c *fiber.Ctx) error { return nil }
+
+// 删除服务器分组
+func DeleteGroup(c *fiber.Ctx) error { return nil }
---
+++ handlers/server.go
@@ -0,0 +1,15 @@
+package handlers
+
+import "github.com/gofiber/fiber/v2"
+
+// 获取服务器列表
+func ServerList(c *fiber.Ctx) error { return nil }
+
+// 更新服务器信息
+func UpdateServer(c *fiber.Ctx) error { return nil }
+
+// 新建服务器
+func CreateServer(c *fiber.Ctx) error { return nil }
+
+// 删除服务器
+func DeleteServer(c *fiber.Ctx) error { return nil }
