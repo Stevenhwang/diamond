958ebbfe67821bd3eb226004038f03835a9f7258
zed <90huanw@gmail.com>
2021-07-16 11:03:47 +0800

    server model

--- models/group.go
+++ models/group.go
@@ -1,6 +1,8 @@
 package models
 
-import "time"
+import (
+	"time"
+)
 
 type Group struct {
 	ID        int
---
+++ models/server.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Server struct {
+	ID        int
+	IP        string         `gorm:"size:128"`
+	Remark    sql.NullString `gorm:"size:128"`
+	Port      int
+	User      string         `gorm:"size:128"`
+	AuthType  int            // 1密码验证 2密钥验证
+	Password  sql.NullString `gorm:"size:128"`
+	Key       sql.NullString `gorm:"type:text"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
