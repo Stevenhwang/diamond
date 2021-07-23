adbf546bf8071ccbaefc0e2041b6fe6cc9825f7c
zed <90huanw@gmail.com>
2021-07-23 22:00:36 +0800

    sizedwaitgroup

---
+++ pools/sizedwaitgroup.go
@@ -0,0 +1,49 @@
+package pools
+
+import (
+	"context"
+	"math"
+	"sync"
+)
+
+type SizedWaitGroup struct {
+	Size    int
+	current chan struct{}
+	wg      sync.WaitGroup
+}
+
+func NewSWG(limit int) SizedWaitGroup {
+	size := math.MaxInt32 // 2^32 - 1
+	if limit > 0 {
+		size = limit
+	}
+	return SizedWaitGroup{
+		Size:    size,
+		current: make(chan struct{}, size),
+		wg:      sync.WaitGroup{},
+	}
+}
+
+func (s *SizedWaitGroup) Add() {
+	s.AddWithContext(context.Background())
+}
+
+func (s *SizedWaitGroup) AddWithContext(ctx context.Context) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case s.current <- struct{}{}:
+		break
+	}
+	s.wg.Add(1)
+	return nil
+}
+
+func (s *SizedWaitGroup) Done() {
+	<-s.current
+	s.wg.Done()
+}
+
+func (s *SizedWaitGroup) Wait() {
+	s.wg.Wait()
+}
