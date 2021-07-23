0acc2014b130aaaf7dbdfc3568bfe2e740ebef80
zed <90huanw@gmail.com>
2021-07-23 18:12:59 +0800

    clean log task

--- crons/init.go
+++ crons/init.go
@@ -10,8 +10,6 @@ import (
 func init() {
 	c := cron.New(cron.WithSeconds(), cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger), cron.Recover(cron.DefaultLogger)))
 	log.Println("开启定时任务")
-	c.AddFunc("@every 5s", func() {
-		log.Println("wow")
-	})
+	c.AddFunc("@daily", CleanLogTask)
 	c.Start()
 }
