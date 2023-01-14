71218b506d6dfc5a60d5baecf615ba4d06078e1a
zed <90huanw@gmail.com>
2023-01-14 18:34:19 +0800

    server filter

--- frontend/src/views/server/index.vue
+++ frontend/src/views/server/index.vue
@@ -66,30 +66,13 @@
     <!-- 弹窗 -->
     <el-header style="margin-top: 5px"
                height="30px">
-      <!-- <el-select v-model="searchKey"
-                 size="small"
-                 clearable
-                 placeholder="请选择搜索项目"
-                 @clear="clearSearchKey">
-        <el-option v-for="item in options"
-                   :key="item.value"
-                   :label="item.label"
-                   :value="item.value" />
-      </el-select>
-      <el-input v-model="searchValue"
+      <el-input v-model="listQuery.query"
                 size="small"
                 style="width:350px;"
+                prefix-icon="el-icon-search"
                 clearable
-                placeholder="请输入搜索内容"
-                @clear="clearSearchValue"
-                @keyup.enter.native="handleFilter" />
-      <el-button class="filter-item"
-                 type="primary"
-                 icon="el-icon-search"
-                 size="small"
-                 @click="handleFilter">
-        搜索
-      </el-button> -->
+                placeholder="请输入搜索内容，支持名称、IP和备注"
+                @input="changeSearch" />&nbsp;
       <el-button type="primary"
                  size="small"
                  icon="el-icon-edit"
@@ -178,7 +161,8 @@ export default {
       total: 0,
       listQuery: {
         page: 1,
-        limit: 15
+        limit: 15,
+        query: ""
       },
       parseTime: parseTime,
       tableData: [],
@@ -206,6 +190,9 @@ export default {
     this.getData()
   },
   methods: {
+    changeSearch() {
+      this.getData()
+    },
     handleConn(row) {
       let routeUrl = this.$router.resolve({
         path: '/terminal',
