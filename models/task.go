51f9381e169ea422d2ec41f0ec79b1732da45287
zed <90huanw@gmail.com>
2023-01-17 23:23:20 +0800

    fix task

--- models/task.go
+++ models/task.go
@@ -7,7 +7,7 @@ import (
 type Task struct {
 	ID        uint      `json:"id"`
 	Name      string    `gorm:"size:256;unique" json:"name" validate:"required"` // 名称
-	Command   string    `gorm:"size:512" json:"command" validate:"required"`     // 执行的命令(脚本单独维护)
+	Command   string    `gorm:"type:text" json:"command" validate:"required"`    // 执行的命令(脚本单独维护)
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
@@ -17,10 +17,10 @@ type Tasks []Task
 type TaskHistory struct {
 	ID        uint      `json:"id"`
 	TaskName  string    `gorm:"size:256" json:"task_name"`
-	User      string    `gorm:"size:128" json:"user"`                    // 执行者
-	FromIP    string    `gorm:"size:128" json:"from_ip"`                 // from IP
-	Success   bool      `json:"success"`                                 // 执行成功、失败
-	Content   string    `gorm:"type:text" json:"remark" filter:"remark"` // 执行内容
+	User      string    `gorm:"size:128" json:"user"`     // 执行者
+	FromIP    string    `gorm:"size:128" json:"from_ip"`  // from IP
+	Success   bool      `json:"success"`                  // 执行成功、失败
+	Content   string    `gorm:"type:text" json:"content"` // 执行结果
 	CreatedAt time.Time `json:"created_at"`
 }
 
